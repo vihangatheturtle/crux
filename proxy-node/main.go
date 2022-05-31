d98e9a95bc2227fb554be947af36e3fc3cc7a291
vihangatheturtle <43118457+vihangatheturtle@users.noreply.github.com>
2022-05-31 03:07:10 +0100

    brhe

--- proxy-node/main.go
+++ proxy-node/main.go
@@ -2,27 +2,39 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"regexp"
+	"strconv"
 
 	"github.com/elazarl/goproxy"
 )
+
+func createBody(currBody []byte) []byte {
+	jObj := map[string]interface{}{
+		"body":             currBody,
+		"bodySignature":    "plain body signature",
+		"lastObjSignature": "last obj signature",
+		"lastBodyHash":     "last body hash",
+	} // TODO: add a body hash
+	res, _ := json.Marshal(jObj)
+	return []byte(res)
+}
 
 func main() {
 	log.Println("Starting proxy server on port 8080")
 	proxy := goproxy.NewProxyHttpServer()
 	proxy.Verbose = true
-	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile("^.*$"))).
-		HandleConnect(goproxy.AlwaysMitm)
-	requestHandleFunc := func(request *http.Request, ctx *goproxy.ProxyCtx) (req *http.Request, resp *http.Response) {
-		req = request
+	proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+	requestHandleFunc := func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+		log.Println(req.Header["Content-Length"])
 		bs, _ := ioutil.ReadAll(req.Body)
-		log.Println("BODY", string(bs))
-		// TODO: modify bs
+		req.Body.Close()
+		bs = createBody(bs)
 		req.Body = ioutil.NopCloser(bytes.NewReader(bs))
-		return
+		req.Header.Set("Content-Length", strconv.Itoa(len(bs)))
+		return req, nil
 	}
 	proxy.OnRequest().DoFunc(requestHandleFunc)
 	log.Fatal(http.ListenAndServe(":8080", proxy))
